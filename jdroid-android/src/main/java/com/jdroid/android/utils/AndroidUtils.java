b5b4b145ae19ff6a4615ea64578a08adc4d76335
Maxi Rosson <maxirosson@gmail.com>
2012-11-02 21:16:45 -0300

    Added missing import

--- jdroid-android/src/main/java/com/jdroid/android/utils/AndroidUtils.java
+++ jdroid-android/src/main/java/com/jdroid/android/utils/AndroidUtils.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import com.jdroid.android.AbstractApplication;
+import com.jdroid.java.utils.FileUtils;
 
 /**
  * 
