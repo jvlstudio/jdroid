08aa09f238e8763f70198c4f94c1774c9099ba6b
Maxi Rosson <maxirosson@gmail.com>
2013-01-17 19:17:14 -0300

    Added Google Maps v2 library

--- jdroid-android/src/main/java/com/jdroid/android/intent/IntentContext.java
+++ jdroid-android/src/main/java/com/jdroid/android/intent/IntentContext.java
@@ -1,6 +1,9 @@
 package com.jdroid.android.intent;
 
 import java.io.Serializable;
+import java.lang.ref.WeakReference;
+import java.util.Map;
+import com.jdroid.java.collections.Maps;
 
 /**
  * 
@@ -8,21 +11,44 @@ import java.io.Serializable;
  */
 public class IntentContext implements Serializable {
 	
-	private static Object data;
+	private static final String DEFAULT_KEY = "defaultKey";
 	
-	public static <T> IntentContext newContext(T data) {
-		IntentContext.data = data;
-		return new IntentContext();
+	private static Map<String, WeakReference<Object>> data = Maps.newHashMap();
+	
+	/**
+	 * @param value The value to add
+	 */
+	public static void addValue(Object value) {
+		addValue(DEFAULT_KEY, value);
 	}
 	
 	/**
-	 * @param <T>
-	 * @return Return the data cleaning it from the context after returning it.
+	 * @param key The data key
+	 * @param value The value to add
+	 */
+	public static void addValue(String key, Object value) {
+		data.put(key, new WeakReference<Object>(value));
+	}
+	
+	/**
+	 * @param key The data key
+	 * @return the data and remove it from the context, so you shouldn't invoke this method twice
 	 */
 	@SuppressWarnings("unchecked")
-	public <T> T getData() {
-		Object returnedData = IntentContext.data;
-		data = null;
-		return (T)returnedData;
+	public static <T> T getValue(String key) {
+		WeakReference<Object> reference = data.get(key);
+		Object value = null;
+		if (reference != null) {
+			value = reference.get();
+			data.remove(key);
+		}
+		return (T)value;
+	}
+	
+	/**
+	 * @return the data and remove it from the context, so you shouldn't invoke this method twice
+	 */
+	public static <T> T getValue() {
+		return getValue(DEFAULT_KEY);
 	}
 }
